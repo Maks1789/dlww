0dc6ab5ef91b7a8c99c243c4d4fa53ea8e54a886
Maks1789 <95221275+Maks1789@users.noreply.github.com>
2022-08-29 17:09:11 +0300

    Update play_logic.py

--- BotFrontend/botFront/play_logic.py
+++ BotFrontend/botFront/play_logic.py
@@ -1,23 +1,32 @@
 import random
-
 from aiogram.dispatcher import FSMContext
-
 from .app import *
 from .data_fetcher import get_word_from_url
 from .keyboards import inline_kb
-from .local_setting import WORDS_API_URL_Street
+from .local_setting import WORDS_API_URL_Street, WORDS_API_URL_Home, WORDS_API_URL_Food
 from .player_state import AnswearState
 
 
+def category_word(data):
+    """result_call - Зберігає категорію, яку обрав користувач """
+    global result_call
+    if data == "street":
+        result_call = WORDS_API_URL_Street
+    elif data == "home":
+        result_call = WORDS_API_URL_Home
+    elif data == "food":
+        result_call = WORDS_API_URL_Food
+    return result_call
 
-@dp.callback_query_handler(text="street", state="*")
+
+@dp.callback_query_handler(lambda c: c.data in ["street", "home", "food"], state="*")
 async def street_play(call: types.CallbackQuery, state: FSMContext):
     """Прои виборі категорії Вулиця, функція отримає іменник з його гендером та зберігає у стані
     AnswearState.street_play_start. Після чого користувачу відображається слово та клавіатура"""
-    await AnswearState.street_play_start.set()          # Set state
-    results = await get_word_from_url(WORDS_API_URL_Street)   # JSON відповідь з бази данних
+    await AnswearState.street_play_start.set()          # Set state зберігає стан
+    results = await get_word_from_url(category_word(call.data)) # JSON відповідь з бази данних
     res = random.choice(results)
-    print(res)
+    print(call.data)
     async with state.proxy() as data:
         data["step"] = 1
         data["answer"] = res.get("gender")
@@ -32,7 +41,7 @@ async def send_random_value(call: types.CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
         print(f"відповідь {answer}")
         if answer == data.get("answer"):
-            res = await get_word_from_url(WORDS_API_URL_Street)
+            res = await get_word_from_url(result_call)
             random_resalt = random.choice(res)
             data["step"] += 1
             data["answer"] = random_resalt.get("gender")
